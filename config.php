067f5ebb2656b07c027ec3550a25ae8b8601ad9b
hisham <hisham.aderis@gmail.com>
2022-12-07 07:24:08 +0800

    added config.php, create.php, delete.php, edit.php and index.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+$collection = (new MongoDB\Client('mongodb://172.17.0.2:27017'))->hddatabase->books;
+
+?>
---
+++ delete.php
@@ -0,0 +1,16 @@
+
+<?php
+
+
+session_start();
+require 'config.php';
+
+
+$collection->deleteOne(['_id' => new MongoDB\BSON\ObjectID($_GET['id'])]);
+
+
+$_SESSION['success'] = "Book deleted successfully";
+header("Location: index.php");
+
+
+?>

b32f37bab6b80dfcc91ab0abec2dd86ac3171d78
hisham <hisham.aderis@gmail.com>
2022-12-07 07:52:28 +0800

    edited the db name

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$collection = (new MongoDB\Client('mongodb://172.17.0.2:27017'))->hddatabase->books;
+$collection = (new MongoDB\Client('mongodb://172.17.0.2:27017'))->booksdb->books;
 
 ?>
